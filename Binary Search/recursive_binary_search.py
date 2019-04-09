c57393df45d30cfffdcb162e4e89fa96fca21b18
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-04-09 14:49:31 -0700

    feat: Slight speed improvement in recursive binary search, with the algorithm returning -1 if the value is not present in the list faster
    Note: Will need to overhaul function later to reduce slicing,
    since it is apparently so inefficient

--- Binary Search/recursive_binary_search.py
+++ Binary Search/recursive_binary_search.py
@@ -5,14 +5,16 @@ def binary_search_recursive(a, x, position=0):
     index = (left+right)//2
     if a[index]==x:
         return position+index
-    if len(a)==1: # case where x is not in the list!
+    elif x>(a[-1]) or x<a[0]: # first case where x is not in the list!
+        return -1
+    elif len(a)==1: # second case where x is not in the list!
         return -1
     elif a[index]<x:
         left = index
         a = a[left:right]
         position+=index # keeps track of the overall index
-        return binary_search(a, x, position)
+        return binary_search_recursive(a, x, position)
     elif a[index]>x:
         right = index
         a = a[left:right]
-        return binary_search(a, x, position)
+        return binary_search_recursive(a, x, position)
