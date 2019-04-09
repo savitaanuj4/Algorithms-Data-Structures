2465df66dc4cfd8f2690173e74794a3275c8dd5d
Patrick Gendotti <patrick.gendotti@gmail.com>
2019-04-09 13:56:42 -0700

    fix: Update binary_search.py to work for Python 3
    Note: Rounding behavior changed from Python 2 to Python 3, so that
    numbers no longer round up on the .5 decimal alone

--- Binary Search/binary_search.py
+++ Binary Search/binary_search.py
@@ -1,3 +1,4 @@
+# Uses python3
 """You're going to write a binary search function.
 You should use an iterative approach - meaning
 using loops.
@@ -11,36 +12,34 @@ Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
 def binary_search(input_array, value):
-    """Your code goes here."""
     test_array = input_array
-    current_index = int(len(input_array)/2)
+    current_index = len(input_array)//2
     input_index = current_index
 
     found_value = test_array[current_index]
     while(len(test_array)>1 and found_value!=value):
         if(found_value<value):
             test_array = test_array[current_index:]
-            current_index = int(len(test_array)/2)
+            current_index = len(test_array)//2
             input_index += current_index
-            found_value = test_array[current_index]
+            found_value = input_array[input_index]
         else:
             test_array = test_array[0:current_index]
-            current_index = int(len(test_array)/2)
-            # had issues with using a '2' instead of a '2.0' on the below
-            # line - need to use decimals to get the precision right and
-            # round correctly!
-            input_index = input_index - int(round(len(test_array)/2.0))
-
-            found_value = test_array[current_index]
-
-    if(found_value==value):
-        return input_index
+            current_index = len(test_array)//2
+            # divmod needed to be used instead of round() since the behavior
+            # for .5 changed from rounding up to rounding down in Python 3
+            q, r = divmod(len(test_array), 2.0)
+            input_index = int(input_index - q - r)
+            found_value = input_array[input_index]
+    else:
+        if(found_value==value):
+            return input_index
 
     return -1
 
 test_list = [1,3,9,11,15,19,29, 35, 36, 37]
 test_val1 = 25
 test_val2 = 15
-print binary_search(test_list, test_val1)
-print binary_search(test_list, test_val2)
-print binary_search(test_list, 11)
+print(binary_search(test_list, test_val1))
+print(binary_search(test_list, test_val2))
+print(binary_search(test_list, 11))
